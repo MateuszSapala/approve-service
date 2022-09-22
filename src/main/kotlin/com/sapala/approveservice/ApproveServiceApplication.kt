86ef3a26f90291c99692075867939fe06a10d27c
Mateusz Sapala <sapala.mateusz@gmail.com>
2022-09-22 19:31:54 +0200

    Initialize application

---
+++ src/main/kotlin/com/sapala/approveservice/ApproveServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.sapala.approveservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApproveServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApproveServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/sapala/approveservice/ApproveServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sapala.approveservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApproveServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
